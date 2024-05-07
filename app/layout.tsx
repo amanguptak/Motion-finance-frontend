0c6276518c8e52edd14eb7063a038ec65f403873
Aman Gupta <eagleax7@gmail.com>
2024-05-08 00:05:09 +0530

    layout improved

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,17 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { Toaster } from 'sonner';
-import Navbar from '@/components/Navbar';
+import { Toaster } from "sonner";
+import Navbar from "@/components/Navbar";
 
-import { Ubuntu } from 'next/font/google';
+import { Ubuntu } from "next/font/google";
 import Header from "@/components/Header";
 
 const ubuntu = Ubuntu({
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-ubuntu',
-  weight: ['300', '400', '500', '700']
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-ubuntu",
+  weight: ["300", "400", "500", "700"],
 });
 
 export const metadata: Metadata = {
@@ -27,15 +27,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={ubuntu.className}>
-     <main>
-    
-    
-  
-      {children}
-      
-      </main>  
+        <main>{children}</main>
         <Toaster position="top-center" richColors />
-        </body>
+      </body>
     </html>
   );
 }
